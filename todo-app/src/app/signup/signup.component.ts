d6f903fdeeacb32c02dc6ccc08a932577c749f46
Kanres-GH <ktnrgs1817@gmail.com>
2023-08-07 16:53:13 +0600

    email verification bug fixed

--- todo-app/src/app/signup/signup.component.ts
+++ todo-app/src/app/signup/signup.component.ts
@@ -14,10 +14,14 @@ export class SignupComponent implements OnInit{
   constructor(private formBuilder: FormBuilder, private http: HttpClient, private router: Router) { }
 
   email = new FormControl('', [Validators.required, Validators.email]);
+
   ngOnInit(): void {
     this.signupForm = this.formBuilder.group({
-      fullName: ['', Validators.required],
-      email: ['', Validators.required],
+      // fullName: ['', Validators.required],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      // email: ['', Validators.required, Validators.email],
+      email: this.email,
       password: ['', Validators.required]
     })
   }
@@ -25,6 +29,7 @@ export class SignupComponent implements OnInit{
   signUp(){
     this.http.post<any>("http://localhost:3000/signupUsers", this.signupForm.value).subscribe(res=>{
       alert("SignUp successful");
+      console.log(this.signupForm.value);
       this.signupForm.reset();
       this.router.navigate(['login']);
     }, err=>{
@@ -32,11 +37,19 @@ export class SignupComponent implements OnInit{
     })
   }
 
+  // Проверка пароля (15 символов)
+
+
+
   getErrorMessage() {
     if (this.email.hasError('required')) {
       return 'You must enter a value';
     }
-
+    console.log(this.email);
     return this.email.hasError('email') ? 'Not a valid email' : '';
   }
+
+  // Email required ^
+
+
 }
